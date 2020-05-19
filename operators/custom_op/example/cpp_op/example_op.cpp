d991cf9b21602919a43294d9d46d6ee3ad9ded63
王志锐5 <wangzhirui5@xdf.cn>
2020-05-19 10:33:53 +0800

    modify note

--- operators/custom_op/example/cpp_op/example_op.cpp
+++ operators/custom_op/example/cpp_op/example_op.cpp
@@ -14,9 +14,10 @@ std::vector<torch::Tensor> example_op_backward(const torch::Tensor& gradOutput) 
 } 
 
 // pybind11 绑定 
-// PYBIND11_MODULE是用来将C++函数绑定到python上的。
-// 其中第一个参数my_op_api为要生成的python模块名，
-// 以后import my_op_api就可以调用该op了。第二个参数固定为m
+// PYBIND11_MODULE 是用来将C++函数绑定到python上的。
+// 其中第一个参数 example_op_api 为要生成的python模块名，
+// 以后import example_op_api 就可以调用该op了。
+// 第二个参数固定为m
 // 函数体中的两个语句分别是绑定前向传播与反向传播到实现的两个函数上。
 PYBIND11_MODULE(example_op_api, m) {
      m.def("forward", &example_op_forward, "example_op forward");
